a6ae65afceb90569729ee88b8ffbf41a72d422e9
gcolossibianchini <gcolossibianchini@gmail.com>
2022-08-22 14:52:42 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/giordan/cursomc/services/CategoriaService.java
+++ src/main/java/com/giordan/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.giordan.cursomc.domain.Categoria;
 import com.giordan.cursomc.repositories.CategoriaRepository;
+import com.giordan.cursomc.services.exceptions.ObjectNotFoundException;
 
 import java.util.Optional;
 
@@ -16,7 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
