287c756f595855f13d556e9a0f1a68e5797c0574
gcolossibianchini <gcolossibianchini@gmail.com>
2022-08-24 15:45:39 -0300

    Listando todas categorias com DTO

--- src/main/java/com/giordan/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/giordan/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,9 @@ package com.giordan.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.giordan.cursomc.domain.Categoria;
+import com.giordan.cursomc.dto.CategoriaDTO;
 import com.giordan.cursomc.services.CategoriaService;
 
 @RestController
@@ -47,6 +51,14 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+		
 	}
 
 }
--- src/main/java/com/giordan/cursomc/services/CategoriaService.java
+++ src/main/java/com/giordan/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.giordan.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir categoria que possui produtos");
 		}
 		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 
 }
