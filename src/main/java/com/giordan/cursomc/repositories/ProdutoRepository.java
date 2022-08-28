f73bef02882785a956e612018648a00babccab2d
gcolossibianchini <gcolossibianchini@gmail.com>
2022-08-28 15:48:34 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/giordan/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/giordan/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,23 @@
 package com.giordan.cursomc.repositories;
 
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.giordan.cursomc.domain.Categoria;
 import com.giordan.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+	
+	@Transactional
+	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn( String nome, List<Categoria> categorias,Pageable pageRequest);
 
 }
